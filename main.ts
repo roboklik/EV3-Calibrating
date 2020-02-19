c5ceb4586500eab77178a8030ccc62b211e61281
RoboKlik <60173099+roboklik@users.noreply.github.com>
2020-02-19 11:03:54 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 
 brick.buttonDown.onEvent(ButtonEvent.Pressed, function () {
-    sensors.color3.onLightDetected(LightIntensityMode.Reflected, Light.Dark, function () {
+    sensors.color2.onLightDetected(LightIntensityMode.Reflected, Light.Dark, function () {
         brick.showString("dark", 2)
     })
-    sensors.color3.onLightDetected(LightIntensityMode.Reflected, Light.Bright, function () {
+    sensors.color2.onLightDetected(LightIntensityMode.Reflected, Light.Bright, function () {
         brick.showString("bright", 2)
     })
     console.sendToScreen()
@@ -12,9 +12,9 @@ brick.buttonDown.onEvent(ButtonEvent.Pressed, function () {
     console.log("and stop moving when done")
     console.log("press ENTER when ready")
     brick.buttonEnter.pauseUntil(ButtonEvent.Pressed)
-    sensors.color3.calibrateLight(LightIntensityMode.Reflected)
-    brick.showValue("dark", sensors.color3.threshold(Light.Dark), 4)
-    brick.showValue("bright", sensors.color3.threshold(Light.Bright), 5)
+    sensors.color2.calibrateLight(LightIntensityMode.Reflected)
+    brick.showValue("dark", sensors.color3.threshold(Light.Dark), 2)
+    brick.showValue("bright", sensors.color3.threshold(Light.Bright), 2)
     forever(function () {
         brick.showValue("reflected light", sensors.color3.light(LightIntensityMode.Reflected), 1)
     })
@@ -25,15 +25,15 @@ brick.buttonDown.onEvent(ButtonEvent.Pressed, function () {
 
     // left -> reset
     brick.buttonLeft.onEvent(ButtonEvent.Pressed, function () {
-        sensors.gyro2.reset()
+        sensors.gyro3.reset()
     })
     // enter -> calibrate
     brick.buttonEnter.onEvent(ButtonEvent.Pressed, function () {
-        sensors.gyro2.calibrate()
+        sensors.gyro3.calibrate()
     })
     // right -> compute drift
     brick.buttonRight.onEvent(ButtonEvent.Pressed, function () {
-        sensors.gyro2.computeDrift()
+        sensors.gyro3.computeDrift()
     })
     // instructions on how to use the buttons
     brick.showString("ENTER: calibrate", 7)
@@ -43,15 +43,25 @@ brick.buttonDown.onEvent(ButtonEvent.Pressed, function () {
     // this loop shows the rate, angle and drift of the
     // robot
     forever(function () {
-        brick.showValue("rate", sensors.gyro2.rate(), 1)
-        brick.showValue("angle", sensors.gyro2.angle(), 2)
-        brick.showValue("drift", sensors.gyro2.drift(), 3)
+
     })
     // this loop shows wheter the sensor is calibrating
     forever(function () {
-        brick.showString(sensors.gyro2.isCalibrating() ? "calibrating..." : "", 4)
+        brick.showString(sensors.gyro3.isCalibrating() ? "calibrating..." : "", 4)
     })
 
+    /*
+    PODPROGRAM ZA IZPIS POMEMBNIH VREDNOSTI SENZORJEV
+    */
+    forever(function () {
+
+        brick.showNumber(sensors.color1.light(LightIntensityMode.Reflected), 1)
+        brick.showNumber(sensors.color2.light(LightIntensityMode.Reflected), 2)
+        brick.showValue("rate", sensors.gyro3.rate(), 3)
+        brick.showValue("angle", sensors.gyro3.angle(), 4)
+        brick.showValue("drift", sensors.gyro3.drift(), 5)
+
+    })
 
 
 
